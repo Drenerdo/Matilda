ffafec83af393da4b5c04f91accc4f6b94bb29ed
princechauhan <princechauhan1424@gmail.com>
2016-02-12 00:00:23 +0530

    news widget

---
+++ src/Matilda.Mirror.GUI/News/NewsModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matilda.Mirror.GUI.News
+{
+   public class NewsModel
+    {
+
+        public class Result
+        {
+            public string section { get; set; }
+            public string subsection { get; set; }
+            public string title { get; set; }
+            public string @abstract { get; set; }
+            public string url { get; set; }
+            public string byline { get; set; }
+            public string item_type { get; set; }
+            public string updated_date { get; set; }
+            public string created_date { get; set; }
+            public string published_date { get; set; }
+            public string material_type_facet { get; set; }
+            public string kicker { get; set; }
+            public object des_facet { get; set; }
+            public object org_facet { get; set; }
+            public object per_facet { get; set; }
+            public object geo_facet { get; set; }
+            public object multimedia { get; set; }
+        }
+        public class ResultForList
+        {
+            public string section { get; set; }
+            public string subsection { get; set; }
+            public string title { get; set; }
+            public string imageurl { get; set; }
+            public string url { get; set; }
+            public string byline { get; set; }
+            public string item_type { get; set; }
+            public string updated_date { get; set; }
+            public string created_date { get; set; }
+            public string published_date { get; set; }
+            public string material_type_facet { get; set; }
+            public string kicker { get; set; }
+            public object des_facet { get; set; }
+            public object org_facet { get; set; }
+            public object per_facet { get; set; }
+            public object geo_facet { get; set; }
+            public object multimedia { get; set; }
+        }
+        public class RootObject
+        {
+            public string status { get; set; }
+            public string copyright { get; set; }
+            public string section { get; set; }
+            public string last_updated { get; set; }
+            public int num_results { get; set; }
+            public List<Result> results { get; set; }
+        }
+        public class MultimediaResult
+        {
+            public string url { get; set; }
+            public string format { get; set; }
+            public int height { get; set; }
+            public int width { get; set; }
+            public string type { get; set; }
+            public string subtype { get; set; }
+            public string caption { get; set; }
+            public string copyright { get; set; }
+
+        }
+        public class Multimedia
+        {
+            public List<MultimediaResult> result { get; set; }
+        }
+    }
+  
+}
---
+++ src/Matilda.Mirror.GUI/News/NewsViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Matilda.Mirror.GUI.News;
+
+namespace Matilda.Mirror.GUI.News
+{
+   public class NewsViewModel: PropertyChangedBase
+    {
+        private ObservableCollection<NewsModel.ResultForList> newsList;
+        public ObservableCollection<NewsModel.ResultForList> NewsList
+        {
+            get
+            {
+                return newsList;
+            }
+            set
+            {
+                if (newsList != value)
+                {
+                    newsList = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+      
+
+        public NewsViewModel()
+        {
+            NewsList = new ObservableCollection<NewsModel.ResultForList>();
+            GetAllNews();
+        }
+        public async void GetAllNews()
+        {
+            try
+            {
+                string provideUri = "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=c2e757d3535346b87e7ed65fd731d384:7:74319570";
+
+                HttpClient client = new HttpClient();
+                string jsonstring = await client.GetStringAsync(provideUri);
+                var response = JsonConvert.DeserializeObject<NewsModel.RootObject>(jsonstring);
+                if (response.status == "OK")
+                {
+                    if(response.results!=null)
+                    {
+                        if(response.results.Count!=0)
+                        {
+                            NewsList.Clear();
+                            foreach (var item in response.results.Take(6))
+                            {
+                                string imageUrl = "ms-appx:///Assets/image_preview.jpg";
+                                if (!string.IsNullOrEmpty(item.multimedia.ToString()))
+                                {
+                                    string data = item.multimedia.ToString().Replace("\r\n","");
+                                    data = "{result :" + data + "}";
+                                    var MultimediaResponse = JsonConvert.DeserializeObject<NewsModel.Multimedia>(data);
+                                    imageUrl = MultimediaResponse.result.LastOrDefault().url;
+                                }
+                                NewsList.Add(new NewsModel.ResultForList()
+                                {
+                                    title = item.title,
+                                    imageurl = imageUrl
+                                });
+                            }
+                        }
+                    }
+                  
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+
+        }
+    }
+}
