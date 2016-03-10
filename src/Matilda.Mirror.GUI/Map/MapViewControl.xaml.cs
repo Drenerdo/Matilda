3a4d3857d41b4d9e0862c2b3bab150dca3fbbc1e
princechauhan <princechauhan1424@gmail.com>
2016-03-11 00:14:17 +0530

    map control

---
+++ src/Matilda.Mirror.GUI/Map/MapViewControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Matilda.Mirror.GUI.Map
+{
+    public sealed partial class MapViewControl : UserControl
+    {
+        public MapViewControl()
+        {
+            this.InitializeComponent();
+            LoadMap();
+        }
+        private async void LoadMap()
+        {
+           
+            Geolocator geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 50;
+            Geoposition pos = await geolocator.GetGeopositionAsync();
+            this.UserMapControl.Center = pos.Coordinate.Point;
+
+        }
+    }
+}
